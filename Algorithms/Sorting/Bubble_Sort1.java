97a7fc6c163d1051d41cab97b4aaacc7598dbf1f
Saomyaraj Jha <jhaamrit630@gmail.com>
2023-11-19 15:08:06 +0530

    Updating Bubble Sort Code

--- Algorithms/Sorting/Bubble_Sort1.java
+++ Algorithms/Sorting/Bubble_Sort1.java
@@ -9,17 +9,19 @@ public class Bubble_Sort1 {
 
     static void Bubble(int[] arr) {
         boolean swapped = false;
-        for (int i=0; i<arr.length; i++) {
-            for (int j=0; j<=arr.length -i; i++) {
-                if (arr[j] <arr[j-1]) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap elements if they are in the wrong order
                     int temp = arr[j];
-                    arr[j] = arr[j-1];
-                    arr[j-1]=temp;
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                     swapped = true;
                 }
             }
             if (!swapped) {
-            break;
+                // If no swaps were made, the array is already sorted
+                break;
             }
         }
     }
